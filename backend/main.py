4268f3c2049fded80216069f5f23a7beb96baf7a
Nurdaulet <nurda0405@gmail.com>
2025-03-16 15:50:09 +0500

    test

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from backend import lifespan, origins, session_middleware_key
 
 
-app = FastAPI(debug=True, lifespan=lifespan)
+app = FastAPI(debug=True, lifespan=lifespan, root_path="/api2")
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True,)
 app.add_middleware(SessionMiddleware, secret_key=session_middleware_key)
 
 if __name__ == "__main__":
-    uvicorn.run(app="backend.main:app", host="fastapi", port=8000, reload=True, root_path="/api")
+    uvicorn.run(app="backend.main:app", host="fastapi", port=8000, reload=True)
