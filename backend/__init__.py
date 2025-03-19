30361b57c3348094408f9f103e70fbc34fe9fec2
Ulan <ulan.sharipov@nu.edu.kz>
2025-03-20 01:28:28 +0500

    auto_ngrok_link_parser

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,11 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-
 from aiogram import Dispatcher, Bot
 from aiogram.fsm.storage.memory import MemoryStorage
+import requests
 
-from backend.routes.bot.config import TG_API_KEY, WEBHOOK_URL
+
+from backend.routes.bot.config import TG_API_KEY, ngrok_server_endpoint, url_webhook_endpoint
 from backend.routes.bot.routes.user import router as user_router
 from backend.routes.bot.routes.user_callback import router as user_callback_router
 from backend.routes.bot.routes.webhook import web_router
@@ -12,6 +13,25 @@ from backend.routes import get_admin, auth, clubs
 from backend.core.database.manager import AsyncDatabaseManager
 from backend.core.configs.config import session_middleware_key
 from backend.routes.auth.auth import KeyCloakManager
+
+def decide_webhook_url(dev_url: ngrok_server_endpoint, prod_url: url_webhook_endpoint, IS_DEBUG: bool = True) -> str:
+    public_url = None
+    if IS_DEBUG:
+        try:
+            response = requests.get(dev_url)
+            response.raise_for_status()
+            tunnels = response.json()["tunnels"]
+            public_url = tunnels[0]["public_url"]
+            print(f"Ngrok public URL: {public_url}")
+        except Exception as e:
+            public_url = None
+            print(f"Error fetching Ngrok URL: {e}")
+    if public_url is not None:
+        url_webhook = f"{public_url}/api/webhook"
+    else:
+        url_webhook = prod_url
+    return url_webhook
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -24,7 +44,8 @@ async def lifespan(app: FastAPI):
         app.state.dp = Dispatcher(storage=MemoryStorage())
         app.state.dp.include_router(user_router)
         app.state.dp.include_router(user_callback_router)
-        await app.state.bot.set_webhook(url=WEBHOOK_URL,
+        url_webhook = decide_webhook_url(dev_url=ngrok_server_endpoint, prod_url=url_webhook_endpoint)
+        await app.state.bot.set_webhook(url=url_webhook,
                                         drop_pending_updates=True,
                                         allowed_updates=app.state.dp.resolve_used_update_types())
 
--- backend/routes/bot/config.py
+++ backend/routes/bot/config.py
@@ -1,7 +1,8 @@
 TG_API_KEY = "8156232977:AAG_Yk2R2nKksdXu35VvDbTaSfztwIYPyrU"
-WEBHOOK_HOST = 'https://ce32-2a03-32c0-3001-e86f-5991-e306-ccec-a839.ngrok-free.app'
-WEBHOOK_PATH = '/api/webhook'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
 
 WEBAPP_HOST = 'localhost'
 WEBAPP_PORT = 3001
+
+ngrok_server_endpoint = "http://127.0.0.1:4040/api/tunnels"
+url_webhook_endpoint = ""
