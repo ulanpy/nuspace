dd66f93f572b77b4f75e7c7804deea820856cc4c
Alan23124 <alansharipov07@gmail.com>
2025-03-21 19:33:36 +0500

    commit before pull 21.03

--- backend/routes/bot/config.py
+++ backend/routes/bot/config.py
@@ -1,5 +1,5 @@
 TG_API_KEY = "8156232977:AAG_Yk2R2nKksdXu35VvDbTaSfztwIYPyrU"
-WEBHOOK_HOST = 'https://ce32-2a03-32c0-3001-e86f-5991-e306-ccec-a839.ngrok-free.app'
+WEBHOOK_HOST = 'https://8c3c-87-255-216-87.ngrok-free.app'
 WEBHOOK_PATH = '/api/webhook'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
