492906cf7ffd67464c7adad9ef7a427128fdfa77
Alan23124 <alansharipov07@gmail.com>
2025-03-24 21:39:13 +0500

    merge 24

--- backend/routes/bot/routes/group.py
+++ backend/routes/bot/routes/group.py
@@ -1,11 +1,10 @@
-
+from datetime import datetime, timedelta
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.enums.chat_type import ChatType
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-# from backend.routes.bot.apsheduler.tasks import kick_user_task
 from backend.routes.bot.keyboards.kb import kb_register_groups
 from backend.routes.bot.cruds import check_user_by_telegram_id
 from backend.celery_app.tasks import schedule_kick
@@ -21,7 +20,7 @@ async def new_member(m: Message,
         return
 
     if not await check_user_by_telegram_id(session=db_session, user_id=m.from_user.id):
-        await m.reply("Зарегайся в NUspace, иначе в течений 15 минут будешь исключен")
+        await m.reply("Зарегайся в NUspace, иначе в течений 15 минут будешь исключен", reply_markup=kb_register_groups())
         run_time = datetime.now() + timedelta(seconds=10)
         # Schedule task with 10 seconds delay
         schedule_kick.apply_async(
