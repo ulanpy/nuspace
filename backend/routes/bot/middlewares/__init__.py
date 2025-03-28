7be5652643696689d945ff584e0370bd4fe237ec
Alan23124 <alansharipov07@gmail.com>
2025-03-28 19:17:56 +0500

    adding init files to set up middlewares and routers in one function

--- backend/routes/bot/middlewares/__init__.py
+++ backend/routes/bot/middlewares/__init__.py
@@ -1,3 +1,14 @@
+from aiogram import Dispatcher
+
 from .db_session import DatabaseMiddleware
 from .redis import RedisMiddleware
 from .public_url import UrlMiddleware
+
+
+def setup_middlewares(dp: Dispatcher):
+    middlewares = [DatabaseMiddleware, RedisMiddleware, UrlMiddleware]
+    for middleware in middlewares:
+        dp.update.middleware(middleware)
+        dp.message.middleware(middleware)
+        dp.callback_query.middleware(middleware)
+        dp.chat_member.middleware(middleware)
--- backend/routes/bot/utils/utils.py
+++ backend/routes/bot/utils/utils.py
@@ -1,15 +1,12 @@
 from aiogram.fsm.storage.memory import MemoryStorage
 import requests
 from aiogram import Dispatcher, Bot
-from aiogram.enums import UpdateType
 
 from fastapi import FastAPI
 
-from backend.routes.bot.middlewares import DatabaseMiddleware, RedisMiddleware, UrlMiddleware
+from backend.routes.bot.middlewares import setup_middlewares
 from backend.routes.bot.config import config
-from backend.routes.bot.routes.user.private.user import router as user_router
-from backend.routes.bot.routes.user.group.group import router as group_router
-from backend.routes.bot.routes.user.private.user_callback import router as user_callback_router
+from backend.routes.bot.routes import include_routers
 
 
 def decide_webhook_url(dev_url: str = config.ngrok_server_endpoint,
@@ -45,33 +42,15 @@ async def initialize_bot(app: FastAPI, token: str = config.TG_API_KEY, dev_url: 
     public_url = url.replace("/api", "")
 
     #Middlewares
-    middlewares = [
-        DatabaseMiddleware(app.state.db_manager),
-        RedisMiddleware(app.state.redis),
-        UrlMiddleware(public_url)
-    ]
-
-    for middleware in middlewares:
-        app.state.dp.update.middleware(middleware)
-        app.state.dp.message.middleware(middleware)
-        app.state.dp.callback_query.middleware(middleware)
-        app.state.dp.chat_member.middleware(middleware)
+    setup_middlewares(app.state.dp)
 
     #Routers
-    app.state.dp.include_router(user_router)
-    app.state.dp.include_router(group_router)
-    app.state.dp.include_router(user_callback_router)
+    include_routers(app.state.dp)
 
-    used_updates = [
-        UpdateType.MESSAGE,
-        UpdateType.CALLBACK_QUERY,
-        UpdateType.CHAT_MEMBER,  # Добавлено
-        UpdateType.MY_CHAT_MEMBER  # Добавлено
-    ]
     print(f"webhook {url}", flush=True)
     await app.state.bot.set_webhook(url=f"{url}/webhook",
                                     drop_pending_updates=True,
-                                    allowed_updates=used_updates)
+                                    allowed_updates=app.state.dp.resolve_used_update_types())
 
 
 
