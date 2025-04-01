7135e2cc9f5121609190b402bf0371ffb08607f8
Ulan <ulan.sharipov@nu.edu.kz>
2025-04-01 12:52:31 +0500

    added media sql model

--- backend/core/database/models/__init__.py
+++ backend/core/database/models/__init__.py
@@ -1,4 +1,5 @@
 from .user import User, UserRole, UserScope
-from .club import Club, ClubManager, ClubEvent, ClubStories, ClubAnnouncement
+from .club import Club, ClubManager, ClubEvent, ClubAnnouncement
 from .base import Base
 from .product import Product, ProductPicture
+from .media import Media
