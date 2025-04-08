cb206d05c26c962877b2a7bf931ffa46b146adda
Ibrahim Tolegen <ibrahim.tolegen06@gmail.com>
2025-04-08 16:56:23 +0500

    Added canteen, category, meal models

--- backend/core/database/models/dormeats.py
+++ backend/core/database/models/dormeats.py
@@ -8,6 +8,28 @@ from enum import Enum
 from datetime import datetime, UTC
 
 class Canteen(Base):
-    pass
-class Menu():
-    pass
+    __tablename__ = 'canteen'
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, nullable=False, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    description: Mapped[str] = mapped_column(nullable=False)
+
+    meals = relationship("Meal", back_populates="canteen")
+
+class Category(Enum):
+    smoothies = "smoothies"
+    salads = "salads"
+    bowls = "bowls"
+    drinks = "drinks"
+    desserts = "desserts"
+    meals = "meals"
+
+class Meal():
+    __tablename__ = 'meals'
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, nullable=False, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    category: Mapped["Category"] = mapped_column(SQLEnum(Category, name='meal_category'), nullable=False) 
+    canteen_id: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    canteen = relationship("Canteen", back_populates="meals")
