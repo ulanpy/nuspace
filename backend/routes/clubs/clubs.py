6a5820dac43ae6fcf3eb51490fba7570b298e3c6
Bekarys <bekarys20050812@gmail.com>
2025-04-11 17:28:21 +0500

    router

--- backend/routes/clubs/clubs.py
+++ backend/routes/clubs/clubs.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, HTTPException, Depends, Response, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from backend.core.database.manager import AsyncDatabaseManager
-from backend.common.dependencies get_db_session
+from backend.common.dependencies import get_db_session
 from typing import Optional, Annotated
 from backend.routes.auth.auth import *
 from .__init__ import *
