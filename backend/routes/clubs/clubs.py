3700b5b079aca0557a0decf90f538f4d1315b15d
Alan23124 <alansharipov07@gmail.com>
2025-04-13 04:25:32 +0500

    adding new clubs router; deleting admin due to changing the models (club), it was dependent on the model

--- backend/routes/clubs/clubs.py
+++ backend/routes/clubs/clubs.py
@@ -6,17 +6,22 @@ from backend.common.dependencies import get_db_session
 from typing import Optional, Annotated, List
 
 from .cruds import add_new_club
-from .schemas import ClubRequestSchema
+from .schemas import ClubRequestSchema, ClubResponseSchema
 
 router = APIRouter(tags=['Club Routes'])
 
 
 @router.post("/clubs/add")
-async def add_club(request: Request, club: ClubRequestSchema, db_session: AsyncSession = Depends(get_db_session)):
+async def add_club(
+    request: Request,
+    club: ClubRequestSchema,
+    db_session: AsyncSession = Depends(get_db_session)
+) -> ClubResponseSchema:
     try:
         return await add_new_club(request, club, db_session)
     except IntegrityError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="db rejected the request: probably there are unique issues")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="db rejected the request: probably there are unique issues")
 
 
 @router.post("/events/add")
