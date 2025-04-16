b6daf649eab22d8c202aaeace967cd1f7145c1f5
Ulan <ulan.sharipov@nu.edu.kz>
2025-04-17 01:56:41 +0500

    autorefresh

--- backend/common/dependencies.py
+++ backend/common/dependencies.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
-from fastapi import Request, HTTPException, status, Cookie, Depends
+from fastapi import Request, HTTPException, status, Cookie, Depends, Response
 from typing import Annotated
 from sqlalchemy import select
 
@@ -19,22 +19,20 @@ async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]
 
 async def check_token(
     request: Request,
-    access_token: Annotated[str | None, Cookie(alias="access_token")] = None
+    response: Response,
+    access_token: Annotated[str | None, Cookie(alias="access_token")] = None,
+    refresh_token: Annotated[str | None, Cookie(alias="refresh_token")] = None
 ) -> dict:
     """Dependency to validate Keycloak token from HTTP-only Secure cookie."""
-    if not access_token:
+    if not access_token or not refresh_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Missing access token cookie"
+            detail="Missing access and/or refresh token cookie"
         )
 
     kc: KeyCloakManager = request.app.state.kc_manager
-    try:
-        return await validate_access_token(access_token, kc)  # Your existing validation
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"Invalid token: {str(e)}")
+    return await validate_access_token(response, access_token, refresh_token, kc)  # Your existing validation
+
 
 
 async def check_tg(
