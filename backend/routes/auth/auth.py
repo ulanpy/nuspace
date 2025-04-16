2f1b2f7e6b4979dc652cbc6a8b20d48b8174a85b
Ulan <ulan.sharipov@nu.edu.kz>
2025-04-17 00:25:24 +0500

    d

--- backend/routes/auth/auth.py
+++ backend/routes/auth/auth.py
@@ -32,6 +32,8 @@ async def auth_callback(request: Request, response: Response, db_session: AsyncS
     await validate_access_token(creds["access_token"], request.app.state.kc_manager)
     user_schema: UserSchema = await create_user_schema(creds)
     await upsert_user(db_session, user_schema)
+    # return creds
+
     frontend_url = f"{config.FRONTEND_HOST}"
     response = RedirectResponse(url=frontend_url, status_code=303)
     set_auth_cookies(response, creds)
