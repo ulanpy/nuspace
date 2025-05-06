c35b055257173b7934d7242f137f0c20f0c16e03
yelnur <ent.elnur.tur@gmail.com>
2025-05-07 01:52:23 +0500

    fixed about

--- frontend/src/components/templates/about-template.tsx
+++ frontend/src/components/templates/about-template.tsx
@@ -15,7 +15,7 @@ export function AboutTemplate() {
         isDarkTheme ? "bg-gray-900 text-gray-100" : "bg-gray-50 text-gray-800"
       }`}
     >
-      <div className="max-w-4xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
+      <div className="max-w-5xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
         <Header />
         <MessionSection />
         <FeatureSection />
