a04b3c7b6232032010375451d0de4a993eca5d7f
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-07-18 19:18:54 +0500

    update

--- frontend/src/components/molecules/slider-container.tsx
+++ frontend/src/components/molecules/slider-container.tsx
@@ -1,6 +1,5 @@
 // components/molecules/SliderContainer.tsx
 import { useRef, useState, ReactNode, useEffect } from "react"; // useEffect қосылды
-import { SliderButton } from "@/components/atoms/slider-button";
 import { Button } from "../atoms/button";
 import { useNavigate } from "react-router-dom";
 
@@ -70,9 +69,6 @@ export function SliderContainer({
         )}
       </div>
       <div className="relative">
-        {canScrollLeft && (
-          <SliderButton direction="left" onClick={() => scroll("left")} />
-        )}
 
         <div
           ref={containerRef}
@@ -81,10 +77,6 @@ export function SliderContainer({
         >
           {children}
         </div>
-
-        {canScrollRight && (
-          <SliderButton direction="right" onClick={() => scroll("right")} />
-        )}
       </div>
     </div>
   );
