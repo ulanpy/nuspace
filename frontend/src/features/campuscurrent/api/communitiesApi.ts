1e4b7150320fd7c1ac34d5119d84c1017b40ea6e
sagyzdop <sagyzdop+github@gmail.com>
2025-08-03 22:50:37 +0500

    we ball

--- frontend/src/features/campuscurrent/api/communitiesApi.ts
+++ frontend/src/features/campuscurrent/api/communitiesApi.ts
@@ -1,25 +1,25 @@
 import { apiCall } from "@/utils/api";
 import { queryOptions } from "@tanstack/react-query";
-import { ROUTES } from "@/data/routes";
+import * as Routes from "@/data/routes";
 import { Community } from "@/features/campuscurrent/types/types";
 export const campuscurrentAPI = {
   getCommunitiesQueryOptions: {
     queryKey: ["campusCurrent", "communities"],
     queryFn: () => {
       return apiCall<Types.PaginatedResponse<Community, "communities">>(
-        `/` + ROUTES.APPS.CAMPUS_CURRENT.COMMUNITIES)
+        `/` + Routes.COMMUNITIES)
     },
   },
   getCommunityQueryOptions: (id: string) => {
     return queryOptions({
       queryKey: ["campusCurrent", "community", id],
       queryFn: () => {
-        return apiCall<Community>(`/` + ROUTES.APPS.CAMPUS_CURRENT.COMMUNITIES+ `/${id}`)
+        return apiCall<Community>(`/` + Routes.COMMUNITIES+ `/${id}`)
       }
     })
   }, 
   editCommunity: (id: string, data: Community) => {
-    return apiCall<Community>(`/` + ROUTES.APPS.CAMPUS_CURRENT.COMMUNITIES+ `/${id}`, {
+    return apiCall<Community>(`/` + Routes.COMMUNITIES+ `/${id}`, {
       method: 'PUT',
       json: data
     })
