c002c46946367ad005a53e13e47316d81a02db20
sagyzdop <sagyzdop+github@gmail.com>
2025-08-12 01:30:59 +0500

    footer

--- frontend/src/components/molecules/footer.tsx
+++ frontend/src/components/molecules/footer.tsx
@@ -1,17 +1,12 @@
-"use client";
-
 import { Link } from "react-router-dom";
 import { ROUTES } from "@/data/routes";
 
-
-const Footer = () => {
+export default function Footer(props) {
   return (
-      <footer className="text-center text-sm text-muted-foreground mt-12 py-4 border-t">
-        <Link to={ROUTES.APPS.ABOUT} className="hover:underline">
-          About Us
-        </Link>
-      </footer>
+    <footer className="text-center text-sm text-muted-foreground mt-12 py-4 border-t">
+      <Link to={ROUTES.APPS.ABOUT} className="hover:underline">
+        {props.note}
+      </Link>
+    </footer>
   );
-};
-
-export default Footer;
+}
