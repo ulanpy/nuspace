1784cc0e11e6969411cf13448e37beb22a252a91
Ulan <ulan.sharipov@nu.edu.kz>
2025-08-14 19:56:22 +0500

    added subspace frontend api

---
+++ frontend/src/features/campuscurrent/subspace/index.ts
@@ -0,0 +1,9 @@
+export * from "./api/subspaceApi";
+export * from "./types";
+export * from "./hooks/usePosts";
+export * from "./hooks/usePost";
+export * from "./hooks/useCreatePost";
+export * from "./hooks/useUpdatePost";
+export * from "./hooks/useDeletePost";
+
+
