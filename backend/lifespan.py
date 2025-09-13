469034158be225a451293bc7a60869d1e724050d
Ulan <ulan.sharipov@nu.edu.kz>
2025-09-14 00:44:18 +0500

    fixed container startup error

--- backend/lifespan.py
+++ backend/lifespan.py
@@ -6,7 +6,7 @@ from backend.app_state.bot import cleanup_bot, setup_bot
 from backend.app_state.db import cleanup_db, setup_db
 from backend.app_state.gcp import setup_gcp
 from backend.app_state.meilisearch import cleanup_meilisearch, setup_meilisearch
-from backend.app_state.rbq import cleanup_rbq, setup_rbq
+# from backend.app_state.rbq import cleanup_rbq, setup_rbq
 from backend.app_state.redis import cleanup_redis, setup_redis
 from backend.core.configs.config import Config
 from backend.routes import routers
@@ -21,7 +21,7 @@ async def lifespan(app: FastAPI):
         app.state.config = Config()  # type: ignore
         app.state.app_token_manager = AppTokenManager()
         setup_gcp(app)
-        await setup_rbq(app)
+        # await setup_rbq(app)
         await setup_db(app)
         await setup_redis(app)
         await setup_meilisearch(app)
@@ -33,7 +33,7 @@ async def lifespan(app: FastAPI):
         yield
 
     finally:
-        await cleanup_rbq(app)
+        # await cleanup_rbq(app)
         await cleanup_bot(app)
         await cleanup_meilisearch(app)
         await cleanup_redis(app)
