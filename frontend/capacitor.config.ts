f7f8a9f624bd8b7354249ca0248004b7f415828b
mciiee <mrchingiz91@gmail.com>
2025-11-03 23:01:25 +0500

    lib: added capacitor.js

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'kz.nuspace',
+  appName: 'nuspace',
+  webDir: 'dist'
+};
+
+export default config;
