7575b9f42a489b0cb517148a7256d83fa0d2a609
mciiee <mrchingiz91@gmail.com>
2025-11-04 02:29:40 +0500

    config: changed nuspace reverse DNS name

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'kz.nuspace',
+  appId: 'kz.nuspace.mobile',
   appName: 'nuspace',
   webDir: 'dist'
 };
