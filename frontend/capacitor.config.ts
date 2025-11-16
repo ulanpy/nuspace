d7bc93443aea72b3e8bb2f67f3a7c00b9e1ee956
mciiee <mrchingiz91@gmail.com>
2025-11-16 20:37:57 +0500

    frontend(capacitor): added an option for custom server

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'kz.nuspace.mobile',
   appName: 'nuspace',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://nuspace.kz',
+    cleartext: true
+  },
 };
 
 export default config;
