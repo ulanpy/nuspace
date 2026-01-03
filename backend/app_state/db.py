521717c13cad7401aaab0199acc015bcabe3c173
Ulan Sharipov <ulan.sharipov@nu.edu.kz>
2026-01-04 01:46:32 +0500

    fix(backend): on config.IS_DEBUG tables created with alembic

--- backend/app_state/db.py
+++ backend/app_state/db.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 
-from backend.core.configs.config import config
+from backend.core.configs.config import config as app_config
 from backend.core.database.manager import AsyncDatabaseManager
-
+from alembic.config import Config
+from alembic import command
 
 async def setup_db(app: FastAPI):
     app.state.db_manager = AsyncDatabaseManager()
-    # Avoid implicit schema creation in production – rely on Alembic migrations instead
-
-    # === When modifying tables, comment this out! ===
-    # if config.IS_DEBUG:
-    #     await app.state.db_manager.create_all_tables()
+    if app_config.IS_DEBUG:
+        config = Config("backend/alembic.ini")
+        command.upgrade(config, "head")
 
 
 async def cleanup_db(app: FastAPI):
